2e3305e85a3ff33d43c3fe8cc4278f9415caec15
unicorn2022 <496300118@qq.com>
2023-12-08 20:30:20 +0800

    修改了文件结构

--- src/main.cpp
+++ src/main.cpp
@@ -1,113 +1,102 @@
 ﻿#pragma warning(disable : 4996)
-#include "../include/EPUB_TO_TXT.h"
-#include "../include/TXT_TO_TXT.h"
-#include "../include/TXT_TO_TXT_Delete_Space.h"
-#include "../include/LRC_TO_ASS.h"
-#include "../include/SRT_TO_ASS.h"
 
+#include "../include/Utils.h"
+#include "../include/main.h"
 
-#include <io.h>
-#include <cstring>
-#include <fstream>
-#include <string>
-#include <vector>
-#include <iostream>
-#include <Windows.h>
-using namespace std;
-
-/*
-path: 指定目录
-files: 保存结果
-fileType: 指定的文件格式，如 .jpg
-*/
-void getAllFiles(string path, vector<string>& files, string fileType);
-
-bool Work();
+bool Work(string command, string fileType, string targetType);
 
 int main() {
-    while (Work());
+    bool firstline = true;
+
+    while (true) {
+        if (!firstline) printf("\n");
+        else firstline = false;
+
+        /* 输入信息 */
+        CommandHelp();
+        // 选择功能
+        string command;
+        SetConsoleColor(ConsoleColor::Yellow);
+        printf("请输入本次要使用的功能, 或者退出程序:");
+        SetConsoleColor(ConsoleColor::Red);
+        cin >> command;
+        if (command.compare("exit") == 0) break;
+
+        string fileType, targetType;
+        if (command.compare("rename") == 0) {
+            // 源文件类型
+            SetConsoleColor(ConsoleColor::Yellow);
+            printf("请输入源文件类型:");
+            SetConsoleColor(ConsoleColor::Red);
+            cin >> fileType;
+            if (fileType.compare("exit") == 0) continue;
+            
+
+            // 目标文件类型
+            SetConsoleColor(ConsoleColor::Yellow);
+            printf("请输入目标文件类型:");
+            SetConsoleColor(ConsoleColor::Red);
+            cin >> targetType;
+            if (targetType.compare("exit") == 0) continue;
+            
+        }
+
+        /* 进行批处理 */
+        while (Work(command, fileType, targetType));
+    }
+
+    SetConsoleColor(ConsoleColor::Clear);
     return 0;
 }
 
-bool Work() {
-    string source;
-    string fileType = "wav";
-    string targetType = "ass";
+bool Work(string command, string fileType, string targetType) {
 
-    /* 输入信息 */
-    {
-        printf("请输入源文件路径, 输入~终止程序:\033[33m");
-        cin >> source;
-        printf("\033[0m");
-
-        if (source.compare("~") == 0) return false;
-
-        printf("请输入源文件类型:\033[33m");
-        cin >> fileType;
-        printf("\033[0m");
-
-        printf("请输入目标文件类型:\033[33m");
-        cin >> targetType;
-        printf("\033[0m");
+    UtilsType type = UtilsType::NONE;
+    for (int i = 0; i < UtilsTypeString.size(); i++) {
+        size_t id = UtilsTypeString[i].find_first_of(" ");
+        string now = UtilsTypeString[i].substr(0, id);
+        if (command.compare(now) == 0)
+            type = static_cast<UtilsType>(i);
     }
 
-    
-    if (source.size() == 0) {
-        // EPUB_TO_TXT::Main();
-        // TXT_TO_TXT::Main();
-        // TXT_TO_TXT_Delete_Space::Main();
-        // LRC_TO_ASS::Main();
-        SRT_TO_ASS::Main();
+    /* 根据指令类型, 设置文件类型 */
+    switch (type) {
+    case UtilsType::EPUB_TO_TXT:
+        fileType = "html";
+        targetType = "txt";
+        break;
+    case UtilsType::TXT_TO_TXT:
+        fileType = "txt";
+        targetType = "txt";
+        break;
+    case UtilsType::TXT_TO_TXT_DELETE_SPACE:
+        fileType = "txt";
+        targetType = "txt";
+        break;
+    case UtilsType::LRC_TO_ASS:
+        fileType = "lrc";
+        targetType = "ass";
+        break;
+    case UtilsType::SRT_TO_ASS:
+        fileType = "srt";
+        targetType = "ass";
+        break;
+    case UtilsType::NONE:
+        SetConsoleColor(ConsoleColor::White);
+        printf("非法指令\n");
+        return false;
     }
-    else {
-        vector<string> files;
-        getAllFiles(source, files, fileType);
 
-        // 对每一个file, 执行一次程序
-        int cnt = 0;
-        for (auto file : files) {
-            /* 重命名文件 */
-            {
-                cnt++;
-                string command = "rename " + file + " Kokomi_" + to_string(cnt) + "." + targetType;
-                system(command.c_str());
-            }
+    string path;
+    SetConsoleColor(ConsoleColor::Yellow);
+    printf("请输入源文件路径:");
+    SetConsoleColor(ConsoleColor::Red);
+    cin >> path;
+    SetConsoleColor(ConsoleColor::Clear);
 
-            /* 文件格式转换 */
-            {
-                size_t pos = file.find_last_of(".");
-                string name = file.substr(0, pos);
-                string target = name + "." + targetType;
+    if (path.compare("exit") == 0) return false;    
 
-                // EPUB_TO_TXT::Main();
-                // TXT_TO_TXT::Main();
-                // TXT_TO_TXT_Delete_Space::Main();
-                // LRC_TO_ASS::Main(file.c_str(), target.c_str());
-                // SRT_TO_ASS::Main(file.c_str(), target.c_str());
-            }
-        }
-    }
+    Utils::Instance().Main(type, path, fileType, targetType);
+
     return true;
-}
-
-
-void getAllFiles(string path, vector<string>& files, string fileType) {
-    // 文件句柄
-    long long hFile = 0;
-    // 文件信息
-    struct _finddata_t fileinfo;
-
-    string p;
-
-    if ((hFile = _findfirst(p.assign(path).append("\\*" + fileType).c_str(), &fileinfo)) != -1) {
-        do {
-            // 保存文件的全路径
-            files.push_back(p.assign(path).append("\\").append(fileinfo.name));
-
-        } while (_findnext(hFile, &fileinfo) == 0);  // 寻找下一个，成功返回0，否则-1
-
-        _findclose(hFile);
-    }
-
-    printf("path: %s, files count = %d\n", path.c_str(), files.size());
 }
